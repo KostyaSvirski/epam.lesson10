3a99e5580140bb63884ae0d2f3c9ed7d4222913f
Kostya <kostyashtc@gmail.com>
2020-09-10 22:53:31 +0300

    fixing

--- src/by/svirski/lesson10/controller/command/impl/FourthTaskCommand.java
+++ src/by/svirski/lesson10/controller/command/impl/FourthTaskCommand.java
@@ -1,7 +1,13 @@
 package by.svirski.lesson10.controller.command.impl;
 
+import java.util.List;
+
 import by.svirski.lesson10.bean.TextItem;
 import by.svirski.lesson10.controller.command.AbstractCommand;
+import by.svirski.lesson10.controller.command.exception.CommandException;
+import by.svirski.lesson10.service.TextService;
+import by.svirski.lesson10.service.exception.ServiceException;
+import by.svirski.lesson10.service.factory.ServiceFactory;
 
 public class FourthTaskCommand implements AbstractCommand {
 
@@ -9,9 +15,16 @@ public class FourthTaskCommand implements AbstractCommand {
 	}
 
 	@Override
-	public TextItem execute(String request) {
-		
-		return null;
+	public TextItem execute(String request) throws CommandException {
+		ServiceFactory factory = ServiceFactory.getInstance();
+		TextService service = factory.getFourthTaskServiceImpl();
+		try {
+			List<? extends TextItem> result = service.executeTask(request);
+			TextItem textItem = new TextItem(result);
+			return textItem;
+		} catch (ServiceException e) {
+			throw new CommandException(e);
+		}
 	}
 
 }
--- src/by/svirski/lesson10/controller/command/impl/SecondTaskCommand.java
+++ src/by/svirski/lesson10/controller/command/impl/SecondTaskCommand.java
@@ -1,7 +1,13 @@
 package by.svirski.lesson10.controller.command.impl;
 
+import java.util.List;
+
 import by.svirski.lesson10.bean.TextItem;
 import by.svirski.lesson10.controller.command.AbstractCommand;
+import by.svirski.lesson10.controller.command.exception.CommandException;
+import by.svirski.lesson10.service.TextService;
+import by.svirski.lesson10.service.exception.ServiceException;
+import by.svirski.lesson10.service.factory.ServiceFactory;
 
 public class SecondTaskCommand implements AbstractCommand{
 
@@ -9,8 +15,16 @@ public class SecondTaskCommand implements AbstractCommand{
 	}
 
 	@Override
-	public TextItem execute(String request) {
-		return null;
+	public TextItem execute(String request) throws CommandException {
+		ServiceFactory factory = ServiceFactory.getInstance();
+		TextService service = factory.getSecondTaskServiceImpl();
+		try {
+			List<? extends TextItem> result = service.executeTask(request);
+			TextItem textItem = new TextItem(result);
+			return textItem;
+		} catch (ServiceException e) {
+			throw new CommandException(e);
+		}
 	}
 
 }
